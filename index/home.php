64b1f00961f6eb8799441d4f8a588d08a733f3e2
Riccardo Spolaor <riccardo.spolaor94@gmail.com>
2018-11-09 13:40:48 +0100

    Update home.php

--- index/home.php
+++ index/home.php
@@ -17,15 +17,7 @@ if (isset($_SESSION['user'])&&$_SESSION['user'] == 'admin') {
 
         <?php
         try {
-            $db = parse_url(getenv("DATABASE_URL"));
-            $database_handler = new PDO ("pgsql:" . sprintf(
-                                        "host=%s;port=%s;user=%s;password=%s';dbname=%s",
-                                         $db["host"],
-                                         $db["port"],
-                                         $db["user"],
-                                         $db["pass"],
-                                         ltrim($db["path"], "/")
-                                        ));
+            $database_handler = new PDO ($dns);
             $database_handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo $e->getMessage();
